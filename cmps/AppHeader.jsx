fa2feb6ac30f1960d842f4c8869c00e56101cd45
eliad <eliad.haim@gmail.com>
2025-02-19 13:29:24 +0200

    Add nav bar btn in mobile

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -3,6 +3,7 @@ const { NavLink } = ReactRouterDOM
 
 export function AppHeader(props) {
 
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
     const headerRef = useRef()
 
     useState(() => {
@@ -20,17 +21,31 @@ export function AppHeader(props) {
         }
     }
 
+    function onMenuBtn() {
+        setIsMenuOpen(prev => prev = !isMenuOpen)
+    }
+
     return (
 
         <header ref={headerRef} className="app-header flex align-center justify-between">
             <h1 className='main-logo'> <NavLink to='/'> Miss Comics</NavLink></h1>
-            <nav className='flex justify-between'>
+
+
+            <input type="checkbox" id='menu' className='menu-input' onChange={onMenuBtn} />
+            <label htmlFor="menu" className='menu-label'>
+                <div className={`menu-btn ${isMenuOpen ? 'open' : 'close'}`}>
+                    <span className={isMenuOpen ? 'fa close' : 'fa bar'}></span>
+                </div>
+            </label>
+
+            <nav className={`flex ${isMenuOpen ? 'open' : 'close'}`} onClick={onMenuBtn}>
                 <NavLink to='/'> Home</NavLink>
                 <NavLink to='/about-us'>About</NavLink>
                 <NavLink to='/books'>Books</NavLink>
                 <NavLink to='/dashboard'>Dashboard</NavLink>
             </nav>
-        </header>
+
+        </header >
 
     )
 }
